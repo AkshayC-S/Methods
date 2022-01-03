9f40e76fd1eab28bf2ecce0fa30f146e615f62b3
AkshayC-S <csakshayxworkz@gmail.com>
2022-01-03 17:32:43 +0530

    uploading a method program using 2 class

---
+++ StudentDetTest.java
@@ -0,0 +1,70 @@
+class StudentDetTest{
+
+		public static void main (String []akcs){
+			
+			StudentDet.addName(0,"Akshat");
+
+            StudentDet.addName(1,"Anita");
+
+            StudentDet.addName(2,"Bharath");
+
+            StudentDet.addName(3,"Deepa");
+
+            StudentDet.addName(4,"Harshith");
+
+            StudentDet.addName(5,"Mahesh");
+
+            StudentDet.addName(6,"Naveen");
+
+            StudentDet.addName(7,"Radhika");
+
+            StudentDet.addName(8,"Rani");
+
+            StudentDet.addName(9,"Santosh");
+
+            StudentDet.addName(10,"Shankar");
+
+            StudentDet.addName(11,"Sheethal");
+
+            StudentDet.addName(12,"Soumya");
+
+            StudentDet.addName(13,"Ullas");
+
+            StudentDet.addName(14,"Umesh");
+
+            StudentDet.addName(15,"Arun");
+
+            StudentDet.addName(16,"Shwetha");
+
+            StudentDet.addName(17,"Muskan");
+
+            StudentDet.addName(18,"Prabhulinga");
+
+            StudentDet.addName(19,"Hemanth");
+
+            StudentDet.addName(20,"Akshay");
+
+            StudentDet.addName(21,"Lakkappa");
+
+            StudentDet.addName(22,"Prashanth");
+
+            StudentDet.addName(23,"Lakshmi");
+
+            StudentDet.addName(24,"Nisarga");
+
+            StudentDet.addName(25,"pooja");
+
+            StudentDet.addName(26,"Lakshmi");
+
+            StudentDet.addName(27,"Manjula");
+
+            StudentDet.addName(28,"Shankar");
+
+            StudentDet.addName(29,"Mohan");
+			
+			StudentDet.printAll();
+			
+			
+		}
+			
+}
