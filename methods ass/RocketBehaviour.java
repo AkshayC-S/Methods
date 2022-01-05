0f480098c0fc63bfed134bf379a1409f0a3b3ed0
AkshayC-S <csakshayxworkz@gmail.com>
2022-01-05 08:33:09 +0530

    Adding assignment 4.2.2022

---
+++ methods ass/RocketBehaviour.java
@@ -0,0 +1,65 @@
+class RocketBehaviour{
+	     static void stage(){
+			 System.out.println("A multistage rocket");
+		 }
+		 static void stage(int stages){
+			 System.out.println("A multistage rocket");
+		 }
+		 static void stage(short stages, String Parallelstages){
+			 System.out.println("A multistage rocket");
+		 }
+		 static void stage(String Parallelstages, short stages){
+			 System.out.println("A multistage rocket");
+		 }
+		 static void payload(){
+			 System.out.println("Anything that a rocket launches into orbit");
+		 }
+		 static void payload(int paylods){
+			 System.out.println("Anything that a rocket launches into orbit");
+		 }
+		 static void payload(short paylods,String Satellite){
+			 System.out.println("Anything that a rocket launches into orbit");
+		 }
+		 static void payload(String Satellite,short payloads){
+			 System.out.println("Anything that a rocket launches into orbit");
+		 }
+		 static void range(){
+			 System.out.println("Reliable attack ranges are 500 to 1000km");
+		 }
+		 static void range(int ranges){
+			 System.out.println("Reliable attack ranges are 500 to 1000km");
+		 }
+		 static void range(short effectivefirerange,String Maximumfirerange){
+			 System.out.println("Reliable attack ranges are 500 to 1000km");
+		 }
+		 static void range(String Maximumfirerange,short effectivefirerange){
+			 System.out.println("Reliable attack ranges are 500 to 1000km");
+		 }
+		 static void speed(){
+			 System.out.println("Any speed of a rocket");
+		 }
+		 static void speed(int speeds){
+			 System.out.println("Any speed of a rocket");
+		 }
+		 static void speed(short speeds,String Highspeed){
+			 System.out.println("Any speed of a rocket");
+		 }
+		 static void speed(String Highspeed,short speeds){
+			 System.out.println("Any speed of a rocket");
+		 }
+		 static void type(){
+			 System.out.println("Any type of seat");
+		 }
+		 static void type(int types){
+			 System.out.println("Any type of seat");
+		 }
+		 static void type(short types,String Chandrayana){
+			 System.out.println("Any type of seat");
+		 }
+		 static void type(String Chandrayana,short types){
+			 System.out.println("Any type of seat");
+		 }
+		 
+		 
+ }
+		 
