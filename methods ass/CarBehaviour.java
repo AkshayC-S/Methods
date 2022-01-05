64d7d8f88f287b2ff09eac4822037b96f4d8bfcb
AkshayC-S <csakshayxworkz@gmail.com>
2022-01-05 08:31:40 +0530

    Adding assignment 4.2.2022

---
+++ methods ass/CarBehaviour.java
@@ -0,0 +1,64 @@
+class CarBehaviour{
+	     static void music(){
+			 System.out.println("Play any musics");
+		 }
+		 static void music(int songs){
+			 System.out.println("Play any musics");
+		 }
+		 static void music(short songs, String Melodies){
+			 System.out.println("Play any musics");
+		 }
+		 static void music(String Melodies, short songs){
+			 System.out.println("Play any musics");
+		 }
+		 static void color(){
+			 System.out.println("Any car color");
+		 }
+		 static void color(int darkcolors){
+			 System.out.println("Any car color");
+		 }
+		 static void color(short darkcolors,String Lightcolors){
+			 System.out.println("Any car color");
+		 }
+		 static void color(String Lightcolors,short darkcolors){
+			 System.out.println("Any car color");
+		 }
+		 static void model(){
+			 System.out.println("Any model of car");
+		 }
+		 static void model(int names){
+			 System.out.println("Any model of car");
+		 }
+		 static void model(short brand,String Kind){
+			 System.out.println("Any model of car");
+		 }
+		 static void model(String Kind,short brand){
+			 System.out.println("Any model of car");
+		 }
+		 static void travel(){
+			 System.out.println("Traveling gadag to banglore");
+		 }
+		 static void travel(int location){
+			 System.out.println("Traveling gadag to banglore");
+		 }
+		 static void travel(short places,String Hills){
+			 System.out.println("Traveling gadag to banglore");
+		 }
+		 static void travel(String Hills,short places){
+			 System.out.println("Traveling gadag to banglore");
+		 }
+		 static void seats(){
+			 System.out.println("Any type of seat");
+		 }
+		 static void seats(int names){
+			 System.out.println("Any type of seat");
+		 }
+		 static void seats(short bucketseat,String Foldingseats){
+			 System.out.println("Any type of seat");
+		 }
+		 static void seats(String Foldingseats,short bucketseat){
+			 System.out.println("Any type of seat");
+		 }
+		 
+		 
+ }
