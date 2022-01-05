78dc04331679a0295c4eab63fee527b2c443df44
AkshayC-S <csakshayxworkz@gmail.com>
2022-01-05 08:32:39 +0530

    Adding assignment 4.2.2022

---
+++ methods ass/CarBehaviourTest.java
@@ -0,0 +1,50 @@
+class CarBehaviourTest{
+	public static void main(String []args){
+		CarBehaviour.music();
+		int index=1;
+		CarBehaviour.music(index);
+		short songs=10;String Melodies="Music";
+		CarBehaviour.music(songs,Melodies);
+		String listen="Music";short lyric=1;
+		CarBehaviour.music(listen,lyric);
+		System.out.println("---------------");
+		
+		CarBehaviour.color();
+		int i=1;
+		CarBehaviour.color(i);
+		short darkcolors=10;String Lightcolors="color";
+		CarBehaviour.color(darkcolors,Lightcolors);
+		String black="";short red=1;
+		CarBehaviour.color(black,red);
+		System.out.println("---------------");
+		
+	    CarBehaviour.model();
+		int j=1;
+		CarBehaviour.model(j);
+		short brand=10;String Kind="model";
+		CarBehaviour.model(brand,Kind);
+		String swift="";short audi=1;
+		CarBehaviour.model(swift,audi);
+		System.out.println("---------------");
+		
+		CarBehaviour.travel();
+		int k=1;
+		CarBehaviour.travel(k);
+		short places=10;String Hills="model";
+		CarBehaviour.travel(places,Hills);
+		String shivagangi="";short nandi=1;
+		CarBehaviour.travel(shivagangi,nandi);
+		System.out.println("---------------");
+		
+		CarBehaviour.seats();
+		int l=1;
+		CarBehaviour.seats(l);
+		short bucketseat=10;String Foldingseats="seats";
+		CarBehaviour.seats(bucketseat,Foldingseats);
+		String power="";short safety=1;
+		CarBehaviour.seats(power,safety);
+		System.out.println("---------------");
+		
+	}
+	
+}
